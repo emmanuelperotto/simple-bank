22647f6a0df1ace3915b85071051e84f5e3fe146
emmanuelperotto <emmanuelperotto@gmail.com>
2022-04-17 03:28:46 -0300

    Run migrations on TestMain

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,6 +2,9 @@ package db
 
 import (
 	"database/sql"
+	"errors"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 	"log"
@@ -24,6 +27,28 @@ func TestMain(m *testing.M) {
 	testDb, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db: ", err)
+	}
+
+	if err := testDb.Ping(); err != nil {
+		log.Fatal("DB connection not alive: ", err)
+	}
+
+	driver, err := postgres.WithInstance(testDb, &postgres.Config{})
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal("Error getting working directory: ", err)
+	}
+
+	migr, err := migrate.NewWithDatabaseInstance(
+		"file://"+dir+"/migrations",
+		"postgres", driver)
+
+	if err != nil {
+		log.Fatal("Error starting migrations process: ", err)
+	}
+
+	if err := migr.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatal("Error applying UP migrations: ", err)
 	}
 
 	testQueries = New(testDb)
